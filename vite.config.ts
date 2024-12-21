e2143930648e48c7b07ddfb020ef0327ce6c5054
Shivam Maheshwari <theshivammaheshwari@gmail.com>
2024-12-21 14:11:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'resume.github.io', // Replace with your repository name
+  base: 'resume', // Replace with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
